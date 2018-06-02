cad419914e74734839988d862ff2d851237a76b3
Andrey <andrey@dpromo.su>
2018-06-02 20:15:09 +0300

    Add tests, Add Readme

--- contracts/AqwireToken.sol
+++ contracts/AqwireToken.sol
@@ -2,10 +2,11 @@ pragma solidity ^0.4.23;
 
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
+
 contract AqwireToken is StandardToken {
-    string public constant name = "AqwireToken"; // solium-disable-line uppercase
-    string public constant symbol = "QEY"; // solium-disable-line uppercase
-    uint8 public constant decimals = 18; // solium-disable-line uppercase
+    string public constant name = "AqwireToken"; 
+    string public constant symbol = "QEY"; 
+    uint8 public constant decimals = 18; 
 
     uint256 public constant INITIAL_SUPPLY = 90000000 * (10 ** uint256(decimals));
 
