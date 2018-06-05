edb3ff4b2bc561fefdaa36c51a7e90006e9cf6a8
Andrey <andrey@dpromo.su>
2018-06-05 13:24:12 +0300

    tokens should not transferable until the crowdsale ends

--- contracts/AqwireToken.sol
+++ contracts/AqwireToken.sol
@@ -9,7 +9,7 @@ contract AqwireToken is StandardToken, Whitelist {
     string public constant symbol = "QEY"; 
     uint8 public constant decimals = 18; 
 
-    uint256 public constant INITIAL_SUPPLY = 90000000 * (10 ** uint256(decimals));
+    uint256 public constant INITIAL_SUPPLY = 180000000 * (10 ** uint256(decimals));
     uint256 public constant unlockTime = now.add(4 weeks);
 
     constructor() public{
@@ -20,6 +20,20 @@ contract AqwireToken is StandardToken, Whitelist {
         // owner is automatically whitelisted
         addAddressToWhitelist(msg.sender);
 
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        // lock transfers until after ICO completes unless whitelisted
+        require(now > unlockTime || whitelist[msg.sender], "Unable to transfer as unlock time not passed or address not whitelisted");
+
+        return super.transfer(_to, _value);
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        // lock transfers until after ICO completes unless whitelisted
+        require(now > unlockTime || whitelist[msg.sender], "Unable to transfer as unlock time not passed or address not whitelisted");
+
+        return super.transferFrom(_from, _to, _value);
     }
 
 }
